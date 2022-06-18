c9535ed91d026fd4fc8fb510c7f981bf27c2124c
JulioSoaresA <juliocsoaresa1@gmail.com>
2022-06-18 20:43:53 -0300

    Criando um formulário de receitas + dados de requisição

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('cadastro', views.cadastro, name='cadastro')
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('criar/receita', views.cria_receita, name='cria_receita')
 ]
